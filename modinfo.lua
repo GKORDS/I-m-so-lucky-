f06eba7288fb880d37f33594fca589f9a8bbd40c
GKORDS <master24939@gmail.com>
2025-11-15 23:45:20 +0700

    Add Winona catapult overdrive mod

---
+++ modinfo.lua
@@ -0,0 +1,16 @@
+name = "Global Winona Catapult Overdrive"
+description = [[
+Extends Winona's catapult capabilities for extreme long-range engagements.
+- Triples the catapult's firing range and target acquisition radius.
+- Allows the Hand Remote to command catapults from virtually anywhere on the map.
+- Maintains compatibility by only adjusting values when the related features exist.
+]]
+author = "ChatGPT"
+version = "1.0.0"
+forumthread = ""
+api_version = 10
+
+dst_compatible = true
+all_clients_require_mod = false
+client_only_mod = false
+
